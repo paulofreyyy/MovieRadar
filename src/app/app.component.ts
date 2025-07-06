2d8b80ccdcedac889230c29f5a252fec667ede16
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-07-06 16:03:26 -0300

    feat: implement movie card styles and details in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [
+        RouterOutlet,
+        MatIconModule,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
